c432f5526e8fd52b785c08507d69ebcbc04c5886
maoxingda <954192476@qq.com>
2019-06-13 17:01:54 +0800

    1.add test json data to file. 2.add test template function.

--- json2cxxstructDemo/json2cxxstructDemo.cpp
+++ json2cxxstructDemo/json2cxxstructDemo.cpp
@@ -7,8 +7,26 @@
 #include <codecvt>
 #include <typeinfo>
 #include "json_jstruct1.json.h"
+#include <fstream>
 //#include <vld.h>
 
+
+template <typename JSTRUCT_TYPE>
+void jstruct_test(JSTRUCT_TYPE jstruct, std::string file_name)
+{
+	std::fstream json(file_name);
+
+	if (json)
+	{
+		std::istreambuf_iterator<char> beg(json), end;
+
+		std::string json_str(beg, end);
+
+		JSTRUCT_TYPE obj;
+
+		assert(obj.from_json(json_str));
+	}
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -20,16 +38,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	assert(stb_false.from_json("{\"field_bool\":false}"));
 	//////////////////////////////////////////////////////////////////////////
-	struct_test_number stn;
-
-	assert(stn.from_json("{\"field_int64\":4611686018427387904,\"field_long\":4611686018427387904,\"field_ushort\":456,\"field_uint\":2147483648,\"field_ulong\":2147483648,\"field_double\":3.1415926,\"field_float\":3.1415926}"));
-
-	//////////////////////////////////////////////////////////////////////////
-	std::wstring_convert < std::codecvt_utf8 < wchar_t >, wchar_t > cnv;
-
-	student stu;
-
-	assert(stu.from_json(cnv.to_bytes(L"{\"identifier\":1001,\"name\":\"Ã«ÐË´ï\",\"qqs\":[954192476],\"qqmail\":[\"954192476@qq.com\",\"15068510522@qq.com\"],\"birthday\":{\"year\":\"1990\",\"month\":\"02\",\"day\":\"16\"},\"birthday_array\":[{\"year\":\"1990\",\"month\":\"02\",\"day\":\"16\"},{\"year\":\"1990\",\"month\":\"02\",\"day\":\"16\"}]}")));
+	jstruct_test(struct_test_number(),	"test/number.json");
+	jstruct_test(student(),				"test/student.json");
 
 	return true;
 }
