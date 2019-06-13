097b366a6651b64952b87ebe65d6e2f3d2f90e34
maoxingda <954192476@qq.com>
2019-06-14 01:10:11 +0800

    1. absolute automation.

--- inc/jstruct.h
+++ inc/jstruct.h
@@ -2,3 +2,11 @@
 #include "jmacro.h"
 #include "jqualifier.h"
 #include "jstruct_base.h"
+
+#ifndef export_jstruct_lib
+#ifdef _DEBUG
+#pragma comment(lib, "json2cxxstructd.lib")
+#else
+#pragma comment(lib, "json2cxxstruct.lib")
+#endif // _DEBUG
+#endif
