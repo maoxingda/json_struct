1099a952223f704767f40d754ff69e1bfbf30bf0
maoxingda <954192476@qq.com>
2019-08-06 00:28:37 +0800

    1. remove xpath

--- inc/jstruct_base.h
+++ inc/jstruct_base.h
@@ -17,7 +17,7 @@ public:
     string to_json();
     bool from_json(string);
 
-    template <typename T> T xpath(string json, string path);
+    //template <typename T> T xpath(string json, string path);
 
 private:
     bool from_json_(void*);
